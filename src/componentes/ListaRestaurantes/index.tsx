7b5a21f1e6d95bf5930cc901375f4a718166087a
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2022-12-28 21:06:13 -0300

    adicionado e configurado prettier

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -1,25 +1,33 @@
 import IRestaurante from '../../interfaces/IRestaurante';
 import style from './ListaRestaurantes.module.scss';
 import Restaurante from './Restaurante';
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const ListaRestaurantes = () => {
-
   const [restaurantes, setRestaurentes] = useState<IRestaurante[]>([]);
 
   useEffect(() => {
-    axios.get('http://localhost:8000/api/v1/restaurantes/').then((resposta) => {
-      setRestaurentes(resposta.data.results);
-    }).catch((err) => {
-      console.log(err);
-    })
+    axios
+      .get('http://localhost:8000/api/v1/restaurantes/')
+      .then((resposta) => {
+        setRestaurentes(resposta.data.results);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
 
-  return (<section className={style.ListaRestaurantes}>
-    <h1>Os restaurantes mais <em>bacanas</em>!</h1>
-    {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
-  </section>)
-}
+  return (
+    <section className={style.ListaRestaurantes}>
+      <h1>
+        Os restaurantes mais <em>bacanas</em>!
+      </h1>
+      {restaurantes?.map((item) => (
+        <Restaurante restaurante={item} key={item.id} />
+      ))}
+    </section>
+  );
+};
 
-export default ListaRestaurantes
+export default ListaRestaurantes;
