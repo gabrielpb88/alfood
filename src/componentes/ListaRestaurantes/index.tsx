478c868f2a7e0f985b54be6feda2c1ee3e2c6b0f
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2022-12-28 22:51:27 -0300

    adicionado botao e comportamento de Ver Mais

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -3,20 +3,40 @@ import style from './ListaRestaurantes.module.scss';
 import Restaurante from './Restaurante';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { IPaginacao } from '../../interfaces/IPaginacao';
 
 const ListaRestaurantes = () => {
   const [restaurantes, setRestaurentes] = useState<IRestaurante[]>([]);
+  const [proximaPagina, setProximaPagina] = useState('');
 
   useEffect(() => {
     axios
-      .get('http://localhost:8000/api/v1/restaurantes/')
+      .get<IPaginacao<IRestaurante>>(
+        'http://localhost:8000/api/v1/restaurantes/',
+      )
       .then((resposta) => {
         setRestaurentes(resposta.data.results);
+        setProximaPagina(resposta.data.next);
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
+
+  function verMais() {
+    axios
+      .get<IPaginacao<IRestaurante>>(proximaPagina)
+      .then((resposta) => {
+        setRestaurentes((prevState) => [
+          ...prevState,
+          ...resposta.data.results,
+        ]);
+        setProximaPagina(resposta.data.next);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
   return (
     <section className={style.ListaRestaurantes}>
@@ -26,6 +46,14 @@ const ListaRestaurantes = () => {
       {restaurantes?.map((item) => (
         <Restaurante restaurante={item} key={item.id} />
       ))}
+      {proximaPagina && (
+        <button
+          onClick={() => {
+            verMais();
+          }}>
+          Ver mais
+        </button>
+      )}
     </section>
   );
 };
